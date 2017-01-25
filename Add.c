ec553a41f72d4fed30f74654143813bc40b42409
Junaid Naeem Khan <jnkhan.8686@gmail.com>
2017-01-25 03:36:31 -0800

    These are C Programs.
    These are C Programs.

---
+++ Add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int a,b;
+   clrscr();
+   printf("\n Enter the 1st no=>");
+   scanf("%d",&a);
+   printf("\n Enter the 2nd no=>");
+   scanf("%d",&b);
+   printf("\n Addition is %d ",(a+b));
+}
